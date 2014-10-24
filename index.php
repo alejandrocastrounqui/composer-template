4d0fd09065a8a09245e8007a24234a59318d8161
alejandro castro <alejandrocastrounqui@gmail.com>
2014-10-24 11:52:51 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+include('vendor/autoload.php');
+$client = new Guzzle\Http\Client();
+$request = $client->get('http://www.google.com');
+
+$result = $request->send();
+print_r($result);
+
+echo 'hola';
+
+?>
